0b6a7d9656a7c1316b7244462aa821af9e61c700
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2020-10-18 12:51:08 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-alert('Any text');
-console.log(123);
+let money = 16000,                          // Доход за месяц
+    income = 'Freelance',                   // Дополнительный доход
+    addExpenses = 'Credit, Oil, Mobile',    // Дополнительный расходы
+    deposit = true,
+    mission = 200000,                       // Цель
+    period = 6,                             // Месяцев
+    budgetDay = Math.round(money / 30);                 // Дневной доход
+    
+
+
+console.log(typeof money);
+console.log(typeof deposit);
+console.log(typeof income);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев'); 
+console.log('Цель заработать ' + mission + ' рублей');
+
+
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
